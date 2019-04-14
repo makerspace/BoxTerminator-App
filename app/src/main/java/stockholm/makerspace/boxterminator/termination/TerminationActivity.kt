f97b089e606dab798880ba275b9c7c4961d675db
Julita Barkauskaite <julita.barkauskaite@daresay.co>
2019-04-14 19:43:11 +0200

    Add burger menu skeleton

--- app/src/main/java/stockholm/makerspace/boxterminator/termination/TerminationActivity.kt
+++ app/src/main/java/stockholm/makerspace/boxterminator/termination/TerminationActivity.kt
@@ -4,8 +4,13 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.burger_menu_activity.*
+import kotlinx.android.synthetic.main.termination_activity.*
 import org.jetbrains.anko.clearTask
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.newTask
@@ -23,16 +28,21 @@ import timber.log.Timber
 
 const val CAMERA_REQUEST = 3333
 
-class TerminationActivity : AppCompatActivity(), TerminationContract.View {
+class TerminationActivity : AppCompatActivity(), TerminationContract.View, NavigationView.OnNavigationItemSelectedListener {
 
     private val presenter : TerminationContract.Presenter by inject { parametersOf(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        terminationBtn.setOnClickListener { startActivityForResult(intentFor<CameraActivity>(),
-            CAMERA_REQUEST
-        ) }
+        setContentView(R.layout.burger_menu_activity)
+        terminationBtn.setOnClickListener { startActivityForResult(intentFor<CameraActivity>(), CAMERA_REQUEST) }
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.burger_open, R.string.burger_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -47,6 +57,15 @@ class TerminationActivity : AppCompatActivity(), TerminationContract.View {
                 }
             }
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_termination -> { }
+            R.id.nav_sessions -> { }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 
     override fun showActiveStatus(member: Member?) {

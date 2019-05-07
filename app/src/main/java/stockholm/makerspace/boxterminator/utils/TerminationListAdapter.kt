a288a25c698020b16e227a5c5fe094aab1ab9ed6
Julita Barkauskaite <julita.barkauskaite@daresay.co>
2019-05-07 21:50:36 +0200

    UI changes for scan list

--- app/src/main/java/stockholm/makerspace/boxterminator/utils/TerminationListAdapter.kt
+++ app/src/main/java/stockholm/makerspace/boxterminator/utils/TerminationListAdapter.kt
@@ -4,9 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.termination_list_item.view.*
+import org.jetbrains.anko.backgroundColor
+import org.jetbrains.anko.backgroundColorResource
 import stockholm.makerspace.boxterminator.R
 import stockholm.makerspace.boxterminator.extensions.inflate
 import stockholm.makerspace.boxterminator.models.Box
+import stockholm.makerspace.boxterminator.models.TerminationStatus
 
 class TerminationListAdapter(private val terminations: List<Box>) : RecyclerView.Adapter<TerminationListAdapter.TerminationViewHolder>() {
 
@@ -25,11 +28,18 @@ class TerminationListAdapter(private val terminations: List<Box>) : RecyclerView
         fun bind(box: Box) {
             view.apply {
                 boxName.text = box.name
-                boxMemberId.text = box.member_number.toString()
+                boxMemberId.text = "#${box.member_number}"
                 boxStatus.text = box.status.status
                 boxExpireDate.text = box.expire_date
                 boxLastNag.text = box.last_nag_at
                 boxLastScan.text = box.last_check_at
+
+                when(box.status){
+
+                    TerminationStatus.ACTIVE -> backgroundColorResource = R.color.memberBoxMembershipValidSecondary
+                    TerminationStatus.EXPIRED -> backgroundColorResource = R.color.memberBoxMembershipInvalidSecondary
+                    TerminationStatus.TERMINATE -> backgroundColorResource = R.color.memberBoxMembershipVeryOldSecondary
+                }
             }
         }
     }
